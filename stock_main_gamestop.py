330c053cabf6b7fee8b3d6a8b0bc358cfd7d94ee
FLUGI69 <167913031+FLUGI69@users.noreply.github.com>
2024-05-29 22:11:10 +0200

    Create stock_main_gamestop.py

---
+++ stock_main_gamestop.py
@@ -0,0 +1,50 @@
+# 3. Extracting GameStop stock data using yfinance #6. GameStop Stock dashboard
+
+class GameStop:
+    def __init__(self, df: pd.DataFrame) -> None:
+
+        self.df = df
+
+    def set_dataframe(self):
+
+        data = yf.Ticker("GME").history(period="max")
+       
+        self.df = pd.DataFrame(data)
+        self.df.reset_index(inplace=True)
+        self.df['Date'] = pd.to_datetime(self.df["Date"], unit="s")
+
+        print(" GAMESTOP STOCK --> \n{}\n".format(self.df.head(10)))
+
+        return self.df
+      
+      def get_fig(self):
+
+        gamestop_stock = "GME"
+
+        self.fig = go.Figure()
+
+        self.fig.add_trace(
+            go.Scatter(
+                x=self.df['Date'], 
+                y=self.df["Close"], 
+                mode='lines', 
+                name=gamestop_stock, 
+                line=dict(color='blue')
+                ))
+        
+        self.fig.update_layout(
+            title=f"Finance {gamestop_stock} Stock",
+            xaxis_title='Date',
+            yaxis_title='Close Price',
+            legend_title='Stocks',
+            template='plotly_white'
+            )
+        
+        return self.fig.show()
+
+if __name__ == "__main__":
+
+    df = pd.DataFrame()
+    gamestop_df = GameStop(df)
+    gamestop_df.set_dataframe()
+    gamestop_df.get_fig()
