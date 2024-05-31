3fddf3b50c87113a6973e620283bb2717edb44b6
FLUGI69 <167913031+FLUGI69@users.noreply.github.com>
2024-05-31 22:17:06 +0200

    Update revenue_t.py

--- revenue_t.py
+++ revenue_t.py
@@ -2,6 +2,7 @@
 import requests
 from bs4 import BeautifulSoup
 import plotly.graph_objs as go
+import pandas as pd
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
@@ -17,10 +18,10 @@ if response.status_code == 200:
     soup = BeautifulSoup(response.text, "html.parser")
     # print(soup.prettify())
 
-    revenue_date = []
-    revenue_price = []
     table = soup.find("table", class_="historical_data_table")
     # print(table)
+
+    df = pd.DataFrame(columns=["Year","Revenue"])
 
     if table:
 
@@ -31,8 +32,7 @@ if response.status_code == 200:
             cells = row.find_all("td")
             year = cells[0].text.strip()
             revenue = cells[1].text.strip()
-            revenue_date.append(year)
-            revenue_price.append(revenue)
+            df.loc[len(df)] = [year, revenue]
 
     else:
         print("Table not found. Check the HTML structure.")
@@ -41,15 +41,16 @@ else:
 
 tesla_revenue = "TSLA Revenue"
 
-sorted_date = sorted(revenue_date)
-sorted_price = revenue_price[::-1]
+df["Revenue"] = df["Revenue"].replace({'\$': ''}, regex=True)
+df["Revenue"] = df["Revenue"].str.replace(',', '').astype(float)
+print(df.tail())
 
 fig = go.Figure()
 
 fig.add_trace(
     go.Bar(
-        x=sorted_date, 
-        y=sorted_price, 
+        x=df["Year"], 
+        y=df["Revenue"], 
         name=tesla_revenue, 
         marker=dict(color='blue')
         ))
@@ -57,7 +58,11 @@ fig.add_trace(
 fig.update_layout(
         title='Tesla Revenue Over Time',
         xaxis_title='Date',
-        yaxis_title='Revenue',
+        yaxis=dict(
+            side='right',
+            title='Revenue',
+            tickvals=[5000, 10000, 15000, 20000, 25000, 30000, 35000, 40000,50000,60000,70000,80000,90000,100000],
+            ticktext=['5B$', '10B$','15B$','20B$','25B$', '30B$', '35B$','40B$','50B$','60B$','70B$','80B$','90B$','100B$']),
         barmode='group'
         )
 
